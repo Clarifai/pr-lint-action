1baddb66a1e7be7168a8f52ec197586e6d4f4ea3
Traver Tischio <8041974+travertischio@users.noreply.github.com>
2024-03-14 13:45:24 -0400

    Allow ready_for_review events to trigger this action

--- index.js
+++ index.js
@@ -92,7 +92,7 @@ Toolkit.run(
       tools.exit.success()
     }
   },
-  { event: ['pull_request.opened', 'pull_request.edited', 'pull_request.synchronize'], secrets: ['GITHUB_TOKEN'] }
+  { event: ['pull_request.opened', 'pull_request.edited', 'pull_request.synchronize', 'pull_request.ready_for_review'], secrets: ['GITHUB_TOKEN'] }
 )
 
 function findFailedCommits(projects, keywords, commitsInPR, ignoreCase) {
